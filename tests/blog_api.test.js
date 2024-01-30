1c22b6f84ef5ab2115572b4189cd045e19d66d3e
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2024-01-30 14:52:58 +0000

    post router test added

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -16,9 +16,37 @@ beforeEach(async () => {
 })
 
 
-test('all notes are returned', async () => {
+test('all blogs are returned', async () => {
     const response = await api.get('/api/blogs')
     expect(response.body).toHaveLength(helper.listWithManyBlogs.length)
+})
+
+test('all blogs have an id property', async () => {
+    const blogs = await helper.blogsInDB()
+    const blogToTest = blogs[0]
+
+    expect(blogToTest.id).toBeDefined()
+})
+
+test('can add a new blog successfully', async () => {
+    const blogToAdd = {
+      title: "Type warlords",
+      author: "Robert C. Martins",
+      url: "https://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 20,
+    };
+
+    await api
+        .post('/api/blogs')
+        .send(blogToAdd)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+    
+    const blogsToEnd = await helper.blogsInDB()
+    expect(blogsToEnd).toHaveLength(helper.listWithManyBlogs.length + 1)
+
+    const titles = blogsToEnd.map(b => b.title)
+    expect(titles).toContain('Type warlords')
 })
 
 
