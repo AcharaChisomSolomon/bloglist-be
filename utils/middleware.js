d3deb018417bb709e3485f0488417daebd2793fa
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2024-02-02 16:03:06 +0000

    extraction of token moved to a middleware

--- utils/middleware.js
+++ utils/middleware.js
@@ -14,6 +14,17 @@ const errorHandler = (error, request, response, next) => {
     next(error)
 }
 
+const tokenExtractor = (request, response, next) => {
+    let authorization = request.get("authorization");
+    if (authorization && authorization.startsWith("Bearer ")) {
+        authorization = authorization.replace("Bearer ", "");
+        request.token = authorization
+    }
+
+    next()
+}
+
 module.exports = {
-    errorHandler
+    errorHandler,
+    tokenExtractor
 }
